67595e4ba9fb4c5387ab8c71cb64e71efe5fe119
madis <madis.pihlasalu@gmail.com>
2023-05-26 09:02:57 +0300

    first commit/add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainViewComponent } from './pages/main-view/main-view.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: MainViewComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), DragDropModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
